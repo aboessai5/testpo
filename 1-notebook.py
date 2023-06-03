13e27a86c2dff02d84aeeb7c80fc5109ef05b72c
aboessai5 <75646501+aboessai5@users.noreply.github.com>
2023-06-03 22:16:57 +0300

    Add files via upload

---
+++ 1-notebook.py
@@ -0,0 +1,173 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "20bbfe4c",
+   "metadata": {},
+   "source": [
+    "Data Science Tools and Ecosystem"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "e048cbf2",
+   "metadata": {},
+   "source": [
+    "In this notebook, Data Science Tools and Ecosystem are summarized.\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "b95f1694",
+   "metadata": {},
+   "source": [
+    "Some of the popular languages that Data Scientists use are:\n",
+    "\n",
+    "1. Python\n",
+    "2. R\n",
+    "3. SQL\n",
+    "4. MATLAB\n",
+    "5. Julia"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "821cf49c",
+   "metadata": {},
+   "source": [
+    "Some of the commonly used libraries used by Data Scientists include:\n",
+    "\n",
+    "1. NumPy - a library for working with arrays and matrices in Python\n",
+    "2. Pandas - a library for data manipulation and analysis in Python\n",
+    "3. Matplotlib - a library for creating visualizations and plots in Python\n",
+    "4. Scikit-learn - a library for machine learning in Python\n",
+    "5. ggplot2 - a library for creating visualizations and plots in R"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "ce809f67",
+   "metadata": {},
+   "source": [
+    "Data Science Tools:\n",
+    "1. Jupyter Notebook     \n",
+    "2. RStudio              \n",
+    "3. Spyder               "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "f58164c9",
+   "metadata": {},
+   "source": [
+    "### Below are a few examples of evaluating arithmetic expressions in Python.\n",
+    "\n",
+    "Arithmetic expressions in Python can be evaluated using standard mathematical operators such as +, -, *, and /, as well as parentheses to indicate order of operations. Here are a few examples:\n",
+    "\n",
+    "- `2 + 3` evaluates to `5`\n",
+    "- `4 * 5 - 2` evaluates to `18`\n",
+    "- `(3 + 4) / 2` evaluates to `3.5`\n",
+    "- `10 / (3 + 1)` evaluates to `2.5`\n",
+    "\n",
+    "Python also supports other mathematical functions and operations through built-in libraries such as NumPy and math."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "ee14886c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "17\n"
+     ]
+    }
+   ],
+   "source": [
+    "#this a simple arithmetic expression to mutiply then add integers\n",
+    "# multiply and add numbers\n",
+    "result = (3*4)+5\n",
+    "print(result)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "ca4ce07d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "3.3333333333333335\n"
+     ]
+    }
+   ],
+   "source": [
+    "#This will convert 200 minutes to hours by diving by 60\n",
+    "# convert minutes to hours\n",
+    "minutes = 200\n",
+    "hours = minutes / 60\n",
+    "print(hours)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "48f07f92",
+   "metadata": {},
+   "source": [
+    "Objectives:\n",
+    "\n",
+    "1. List popular languages for Data Science\n",
+    "2. Identify the R library commonly used for machine learning\n",
+    "3. Describe the concept of a fork in GitHub\n",
+    "4. Explain how GitHub provides streamlined testing and delivery\n",
+    "5. Demonstrate how to add a file to a GitHub repository from your local machine\n",
+    "6. Create a markdown cell with a table of Data Science tools\n",
+    "7. Demonstrate how to evaluate arithmetic expressions in Python\n",
+    "8. Convert minutes to hours using Python"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "d9bc756d",
+   "metadata": {},
+   "source": [
+    "My name is Ahmed, and I am computer engneering .  If you have any questions or feedback, feel free to ask!"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d3c8e6d8",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
